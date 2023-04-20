14600bcbf6c12ac7ce6a13cf384822573896bbf2
yal103 <yal103@ucsd.edu>
2023-04-20 16:44:28 -0700

    fixes reverseInPlace and reversed methods

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,18 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  //own
+  @Test
+  public void testReverseInPlaceFail() {
+    int[] input1 = {1, 2, 3, 4, 5};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{5, 4, 3, 2, 1}, input1);
+  }
+
+  @Test
+  public void testReversedFail() {
+    int[] input1 = {1, 2, 3};
+    assertArrayEquals(new int[]{3, 2, 1}, ArrayExamples.reversed(input1));
+  }
 }
